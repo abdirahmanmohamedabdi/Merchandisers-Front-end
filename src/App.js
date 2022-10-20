3b44e54e76ee4739c6f07fbee14d5d05ddf85ace
tangusjr <kipngenogilbert96@gmail.com>
2022-10-20 21:26:40 +0300

    created and modified the user signup form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import {Route, Routes} from 'react-router-dom';
-import SignupAdmin from './components/SignupAdmin';
+// import SignupAdmin from './components/SignupAdmin';
+import UserSignup from './components/UserSignup';
 
 function App() {
   return (
     <div className="App">
-  <SignupAdmin />
+  {/* <SignupAdmin /> */}
+  <UserSignup />
     </div>
   );
 }
--- src/components/UserSignup.js
+++ src/components/UserSignup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function SignupAdmin({ setadmin }) {
+function SignupUser({ setadmin }) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -8,7 +8,7 @@ function SignupAdmin({ setadmin }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/signup_admin", {
+    fetch("/signup_user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -16,7 +16,7 @@ function SignupAdmin({ setadmin }) {
       body: JSON.stringify({ username, email, password, passwordConfirmation }),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((admin) => setadmin(admin));
+        r.json().then((user) => setadmin(user));
       }
     });
   }
@@ -25,7 +25,7 @@ function SignupAdmin({ setadmin }) {
     <div>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-        <h1>Admin Signup</h1>
+        <h1>User Signup</h1>
         <label htmlFor="username" className="form-label">Username</label>
         <input
           type="text"
@@ -67,4 +67,4 @@ function SignupAdmin({ setadmin }) {
   );
 }
 
-export default SignupAdmin
+export default SignupUser;
