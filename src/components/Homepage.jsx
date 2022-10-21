1418b77bdf20ffe59bdb78199e74c3d834c02856
Abdirahman <abdirahmanabdi.ma@gmail.com>
2022-10-21 17:09:49 +0300

    made some changes

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import LoginUser from './LoginUser'
+import Home from './Navbar'
+function Homepage ()
+    {
+
+    
+  return (
+    <div>
+        <Home/>
+        
+    <div className="random">
+  <div className="container">
+    <h1 className="display-6">Mash-Industries</h1>
+    <p className="lead">Mash industries Ltd is a company manufacturing Fast-Moving Consumer Goods (FMCG)  such as Soap, Cooking Oil, etc. The company sales’ department has hundreds of salespeople named merchandisers reporting about their product insights to the HQ on a daily basis.
+</p>
+  </div>
+</div>
+</div>
+  )
+}
+export default Homepage;
